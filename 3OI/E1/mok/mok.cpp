f470aecddb97364c0f12cbab564a0ba131ca0b9b
kidp330 <j.migdal42@gmail.com>
2025-09-07 12:03:02 +0200

    [mok] Fixed compilation errors

--- 3OI/E1/mok/mok.cpp
+++ 3OI/E1/mok/mok.cpp
@@ -5,9 +5,9 @@ typedef array<int, N> arr;
 
 vector<int> d(6250000);
 
-void insert(const int &x, const int &st, queue<int> &Q, unordered_map<int,int> &d) {
+void insert(const int &x, const int &st, queue<int> &Q) {
   if (d[x] == 0) {
-    d[x] = d[st]+1;
+    d[x] = d[st] + 1;
     Q.push(x);
   }
 }
@@ -15,21 +15,21 @@ void insert(const int &x, const int &st, queue<int> &Q, unordered_map<int,int> &
 int factor(int x, arr &st) {
   int s = 0;
   for (int i = 0; i < N; ++i) {
-    st[i] = x%50;
+    st[i] = x % 50;
     s += st[i];
     x /= 50;
   }
   return s;
 }
 
-int bfs(const int &n, arr const& cont, arr const& res) {
+int bfs(const int &n, arr const &cont, arr const &res) {
   queue<int> Q;
   arr pow = {1, 50, 2500, 125000};
   int cont_50 = 0, res_50 = 0, res_s = 0;
 
   for (int i = 0; i < n; ++i) {
-    cont_50 += cont[i]*pow[i];
-    res_50 += res[i]*pow[i];
+    cont_50 += cont[i] * pow[i];
+    res_50 += res[i] * pow[i];
     res_s += res[i];
   }
   d[cont_50] = 1;
@@ -38,37 +38,38 @@ int bfs(const int &n, arr const& cont, arr const& res) {
   Q.push(cont_50);
 
   while (!Q.empty()) {
-    auto st_50 = Q.front(); Q.pop();
+    auto st_50 = Q.front();
+    Q.pop();
     if (st_50 == res_50)
-      return d[st_50]-1;
+      return d[st_50] - 1;
 
     arr st;
     int s = factor(st_50, st);
 
     for (int i = 0; i < n; ++i) {
-      for (int j = i+1; j < n; ++j) {
+      for (int j = i + 1; j < n; ++j) {
         // op1.
         // i -> j
         if (st[i] + st[j] <= cont[j]) {
-          insert(st_50 + st[i]*(pow[j] - pow[i]), st_50, Q, d);
+          insert(st_50 + st[i] * (pow[j] - pow[i]), st_50, Q);
         }
         // j -> i
         if (st[i] + st[j] <= cont[i]) {
-          insert(st_50 + st[j]*(pow[i] - pow[j]), st_50, Q, d);
+          insert(st_50 + st[j] * (pow[i] - pow[j]), st_50, Q);
         }
         // op2.
         // fill i
-        if (st[j] >= cont[i]-st[i] && st[i] != cont[i]) {
-          insert(st_50 - (pow[j] - pow[i])*(cont[i] - st[i]), st_50, Q, d);
+        if (st[j] >= cont[i] - st[i] && st[i] != cont[i]) {
+          insert(st_50 - (pow[j] - pow[i]) * (cont[i] - st[i]), st_50, Q);
         }
         // fill j
-        if (st[i] >= cont[j]-st[j] && st[j] != cont[j]) {
-          insert(st_50 + (pow[j] - pow[i])*(cont[j] - st[j]), st_50, Q, d);
+        if (st[i] >= cont[j] - st[j] && st[j] != cont[j]) {
+          insert(st_50 + (pow[j] - pow[i]) * (cont[j] - st[j]), st_50, Q);
         }
       }
       // op3.
       if (s - st[i] >= res_s) {
-        insert(st_50 - st[i]*pow[i], st_50, Q, d);
+        insert(st_50 - st[i] * pow[i], st_50, Q);
       }
     }
   }
@@ -80,7 +81,8 @@ int main() {
   cin.tie(0);
   cout.tie(0);
 
-  int n; cin >> n;
+  int n;
+  cin >> n;
   arr cont = {0, 0, 0, 0}, res = cont;
   for (int i = 0; i < n; ++i)
     cin >> cont[i];
